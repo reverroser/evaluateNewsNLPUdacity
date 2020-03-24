6aade11b5a8f97727de7285a6ca7aded2bb3c388
Roser Reverte <reverroser@gmail.com>
2020-03-24 17:49:14 +0100

    project set up

---
+++ src/client/index.js
@@ -0,0 +1,8 @@
+import './styles/resets.scss'
+import { checkForName } from './js/nameChecker'
+import { handleSubmit } from './js/formHandler'
+
+console.log(checkForName);
+
+alert("I EXIST")
+console.log("CHANGE!!");
